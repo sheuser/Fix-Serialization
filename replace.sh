f207f3d23f4e4ffb400cf0924fb05ba4799a4598
R. Brenton Strickler <rbrenton@gmail.com>
2017-04-05 10:51:25 -0400

    Clean up replace.sh example.

--- replace.sh
+++ replace.sh
@@ -1,17 +1,11 @@
-echo 'Replacing ...';
+#!/bin/bash -e
 
-# Enum sql files
-for f in *.sql;	do
-	echo 'File:' $f;
-	sed 's/mydomain\.com/mynewdomain\.com/g' $f > $f.tmp; mv $f.tmp $f;
+# Enum sql files and fix serialization.
+echo "Search and replace, and fixing serialization for '*.sql'."
+for f in `ls *.sql 2> /dev/null`
+do
+  echo "File: $f"
+  sed 's/mydomain\.com/mynewdomain\.com/g' $f > $f.tmp && /usr/bin/php fix-serialization $f.tmp && mv $f.tmp $f
 done
 
-echo 'Fix serialization ...';
-
-# Fix serialized length strings
-for f in *.sql;	do
-	echo 'File:' $f;
-	/usr/bin/php fix-serialization.php $f
-done
-
-echo 'Done!';
+echo "Done!"
