21c85d7174f2c2e8be7f945e81aea995f4390676
R. Brenton Strickler <rbrenton@gmail.com>
2017-04-05 10:25:52 -0400

    Clean up spacing, remove tabs. Implement exception handling. Note path handing is now relative.

--- fix-serialization.php
+++ fix-serialization.php
@@ -2,25 +2,25 @@
 <?php
 
 /*
-* Blogestudio Fix Serialization	1.2
+* Blogestudio Fix Serialization  1.2
 * Fixer script of length attributes for serialized strings (e.g. Wordpress databases)
 * License: GPL version 3 or later - http://www.gnu.org/licenses/gpl.txt
 * By Pau Iglesias
 * http://blogestudio.com
-* 
+*
 * Inspiration and regular expression code base from David Coveney:
 * http://davidcoveney.com/575/php-serialization-fix-for-wordpress-migrations/
-* 
+*
 * Usage:
 *
-* 	/usr/bin/php fix-serialization.php my-sql-file.sql
+*   /usr/bin/php fix-serialization.php my-sql-file.sql
 *
 * Versions:
-* 
-* 	1.0 2011-08-03 Initial release
-* 	1.1 2011-08-18 Support for backslashed quotes, added some code warnings
-* 	1.2 2011-09-29 Support for null or zero length strings after preg_replace is called, and explain how to handle these errors
-* 
+*
+*   1.0 2011-08-03 Initial release
+*   1.1 2011-08-18 Support for backslashed quotes, added some code warnings
+*   1.2 2011-09-29 Support for null or zero length strings after preg_replace is called, and explain how to handle these errors
+*
 * Knowed errors:
 *
 * - Memory size exhausted
@@ -30,26 +30,26 @@
 * - Function preg_replace returns null or 0 length string
 * If preg_last_error = PREG_BACKTRACK_LIMIT_ERROR (value 2), increase pcre.backtrack_limit in php.ini (by default 100k, change to 2M by example)
 * Same way for others preg_last_error codes: http://www.php.net/manual/en/function.preg-last-error.php
-* 
+*
 * TODO next versions
-* 
+*
 * - Check if needed UTF-8 support detecting and adding u PCRE modifier
-* 
+*
 */
 
 
 
 // Unescape to avoid dump-text issues
 function unescape_mysql($value) {
-	return str_replace(array("\\\\", "\\0", "\\n", "\\r", "\Z",  "\'", '\"'),
-					   array("\\",   "\0",  "\n",  "\r",  "\x1a", "'", '"'), 
-					   $value);
+  return str_replace(array("\\\\", "\\0", "\\n", "\\r", "\Z",  "\'", '\"'),
+                     array("\\",   "\0",  "\n",  "\r",  "\x1a", "'", '"'),
+                     $value);
 }
 
 function escape_mysql($value) {
-	return str_replace(array("\\",   "\0",  "\n",  "\r",  "\x1a", "'", '"'),
-					   array("\\\\", "\\0", "\\n", "\\r", "\Z",  "\'", '\"'),
-					   $value);
+  return str_replace(array("\\",   "\0",  "\n",  "\r",  "\x1a", "'", '"'),
+                     array("\\\\", "\\0", "\\n", "\\r", "\Z",  "\'", '\"'),
+                     $value);
 }
 
 function preg_cb($m, $mysql) {
@@ -69,106 +69,69 @@ function cb2_plain($m) {
 }
 
 
-// Check command line arguments
-if (!(isset($argv) && isset($argv[1]))) {
-	
-	// Error
-	echo 'Error: no input file specified'."\n\n";
+try {
+  // Check command line arguments.
+  if (!is_array($argv) || !isset($argv[1])) {
+    throw new Exception("No input file specified.");
+  }
 
-// With arguments
-} else {
-	
-	// Compose path from argument
-	//$path = dirname(__FILE__).'/'.$argv[1];
-	$path = $argv[1];
-	if (!file_exists($path)) {
-	
-		// Error
-		echo 'Error: input file does not exists'."\n";
-		echo $path."\n\n";
-	
-	// File exists
-	} else {
-	
-		// Get file contents
-		if (!($fp = fopen($path, 'r'))) {
-			
-			// Error
-			echo 'Error: can`t open input file for read'."\n";
-			echo $path."\n\n";
-		
-		// File opened for read
-		} else {
-			
-			// Initializations
-			$do_preg_replace = false;
-		
-			// Copy data
-			if (!($data = fread($fp, filesize($path)))) {
+  // Compose path from argument.
+  $path = $argv[1]; // Handle relative paths.
+  if (!file_exists($path)) {
+    throw new Exception("Input file does not exist: $path");
+  }
 
-				// Error
-				echo 'Error: can`t read entire data from input file'."\n";
-				echo $path."\n\n";
-			
-			// Check data
-			} elseif (!(isset($data) && strlen($data) > 0)) {
+  // File exists
 
-				// Warning
-				echo "Warning: the file is empty or can't read contents\n";
-				echo $path."\n\n";
-			
-			// Data ok
-			} else {
+  // Get file contents
+  if (!($fp = fopen($path, 'r'))) {
+    throw new Exception("Can`t open input file for reading: $path");
+  }
 
-				// Tag context
-				$do_preg_replace = true;
+  // File opened for read
 
-				// Replace serialized string values
-				$data = preg_replace_callback('/s:(\d+):([\\\\]"[\\\\]"|[\\\\]"((.*?)[^\\\\])[\\\\]");(?=($|[}]|[a-z]:\d+|[\'][)]))/', "cb1_mysql", $data);
-				$data = preg_replace_callback('/s:(\d+):(""|"(.*?)");(?=($|[}]|[a-z]:\d+))/', "cb2_plain", $data);
-			}
+  // Copy data
+  if (!($data = fread($fp, filesize($path)))) {
+    throw new Exception("Can`t read entire data from input file: $path");
+  }
 
-			// Close file
-			fclose($fp);
-			
-			// Check data
-			if (!(isset($data) && strlen($data) > 0)) {
-				
-				// Check origin
-				if ($do_preg_replace) {
+  // Check data
+  if (!(isset($data) && strlen($data) > 0)) {
+    throw new Exception("The file is empty or can't read contents: $path");
+  }
 
-					// Error
-					echo "Error: preg_replace returns nothing\n";
-					if (function_exists('preg_last_error')) echo "preg_last_error() = ".preg_last_error()."\n";
-					echo $path."\n\n";
-				}
-			
-			// Data Ok
-			} else {
+  // Data ok
 
-				// And finally write data
-				if (!($fp = fopen($path, 'w'))) {
+  // Replace serialized string values
+  $data = preg_replace_callback('/s:(\d+):([\\\\]"[\\\\]"|[\\\\]"((.*?)[^\\\\])[\\\\]");(?=($|[}]|[a-z]:\d+|[\'][)]))/', "cb1_mysql", $data);
+  $data = preg_replace_callback('/s:(\d+):(""|"(.*?)");(?=($|[}]|[a-z]:\d+))/', "cb2_plain", $data);
 
-					// Error
-					echo "Error: can't open input file for writing\n";
-					echo $path."\n\n";
-					
-				// Open for write
-				} else {
-					
-					// Write file data
-					if (!fwrite($fp, $data)) {
-						
-						// Error
-						echo "Error: can't write input file\n";
-						echo $path."\n\n";
-					}
-					
-					// Close file
-					fclose($fp);
-				}
-			}
-		}
-	}
+  // Close file
+  fclose($fp);
+
+  // Check data
+  if (!(isset($data) && strlen($data) > 0)) {
+    $preg_error = function_exists('preg_last_error') ? preg_last_error() : '';
+    throw new Exception("preg_replace returns nothing\n$preg_error");
+  }
+
+  // Data Ok
+
+  // And finally write data
+  if (!($fp = fopen($path, 'w'))) {
+    throw new Exception("Can't open input file for writing: $path");
+  }
+
+  // Open for write
+
+  // Write file data
+  if (!fwrite($fp, $data)) {
+    throw new Exception("Can't write input file: $path");
+  }
+
+  // Close file
+  fclose($fp);
 }
-
+catch (Exception $e) {
+  echo "ERROR: " . trim($e->getMessage()) . "\n";
+}
