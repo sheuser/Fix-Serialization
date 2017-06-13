7990608db35974d43f3865679bd3b4d4b28ae52c
Sýlvan Heuser <Sylvan.Heuser@flexoptix.net>
2017-06-13 09:39:44 +0200

    Clean up replace.sh
    ...and use sed's in-place flag, without the manual temporary file
    handling. If you don't keep backups, I have no sympathy for you.

--- replace.sh
+++ replace.sh
@@ -1,11 +1,9 @@
 #!/bin/bash -e
 
-# Enum sql files and fix serialization.
 echo "Search and replace, and fixing serialization for '*.sql'."
-for f in `ls *.sql 2> /dev/null`
-do
+for file in *.sql; do
   echo "File: $f"
-  sed 's/mydomain\.com/mynewdomain\.com/g' $f > $f.tmp && /usr/bin/php fix-serialization $f.tmp && mv $f.tmp $f
+  sed -i 's/mydomain\.com/mynewdomain\.com/g' "$file" && /usr/bin/php fix-serialization.php "$file"
 done
 
 echo "Done!"
